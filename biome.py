ff169ee4ea17602074758c58409a17c29a9df0e6
Reuben <reubendinny@gmail.com>
2022-12-03 10:06:29 +0530

    varun stupid

--- biome.py
+++ biome.py
@@ -1,5 +1,6 @@
 import customtkinter, tkinter
 from math import trunc
+from PIL import Image
 
 
 class App(customtkinter.CTk):
@@ -20,13 +21,21 @@ class App(customtkinter.CTk):
         self.idealT = 27.5
         self.idealL = 13
 
-        self.geometry("600x300")
+
+        self.my_image1 = customtkinter.CTkImage(light_image=Image.open("D:\Studies\Engineering\Hackus2\python_gui\images\hand.png"), size=(30, 30))
+        self.my_image2 = customtkinter.CTkImage(light_image=Image.open("D:\Studies\Engineering\Hackus2\python_gui\images\peas.jpg"), size=(30, 30))
+        self.my_image3 = customtkinter.CTkImage(light_image=Image.open("D:\Studies\Engineering\Hackus2\python_gui\images\hand2.jpeg"), size=(30, 30))
+
+        self.db = [["Tomato",75, 27, 13,self.my_image1], ["Peas", 90, 22, 5, self.my_image2], ["brinjal", 60, 24, 7,self.my_image3]]
+        self.plant = 0
+
+        self.geometry("600x400")
         self.title("small example app")
         self.minsize(300, 200)
 
         # create 2x2 grid system
-        self.grid_rowconfigure(3, weight=0)
-        self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.grid_rowconfigure(7, weight=0)
+        self.grid_columnconfigure((0, 1, 2, 3), weight=1)
 
         self.envlabel = customtkinter.CTkLabel(master=self, text="Environment")
         self.envlabel.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0), sticky="nsew")
@@ -45,10 +54,10 @@ class App(customtkinter.CTk):
         self.slider1 = customtkinter.CTkSlider(master=self, from_=0, to=100, command=self.slider_humid)
         self.slider1.grid(row=1, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
 
-        self.slider2 = customtkinter.CTkSlider(master=self, from_=0, to=100, command=self.slider_temp)
+        self.slider2 = customtkinter.CTkSlider(master=self, from_=-20, to=70, command=self.slider_temp)
         self.slider2.grid(row=2, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
 
-        self.slider3 = customtkinter.CTkSlider(master=self, from_=0, to=100, command=self.slider_light)
+        self.slider3 = customtkinter.CTkSlider(master=self, from_=0, to=24, command=self.slider_light)
         self.slider3.grid(row=3, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
 
         self.label4 = customtkinter.CTkLabel(master=self, text="Humidity : " + str(round(self.Chumid,2)))
@@ -60,7 +69,41 @@ class App(customtkinter.CTk):
         self.label6 = customtkinter.CTkLabel(master=self, text="Light : " + str(round(self.Clight,2)))
         self.label6.grid(row=3, column=2, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
         
+        self.label7 = customtkinter.CTkLabel(master=self, text="Required Humidity : " + str(round(self.idealH,2)))
+        self.label7.grid(row=4, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+        self.label8 = customtkinter.CTkLabel(master=self, text="Required Temperature : " + str(round(self.idealT,2)))
+        self.label8.grid(row=5, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+        self.label9 = customtkinter.CTkLabel(master=self, text="Required Light : " + str(round(self.idealL,2)))
+        self.label9.grid(row=6, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+
+
+        self.button1 = customtkinter.CTkButton(master=self, text=self.db[self.plant][0],image=self.db[self.plant][4], command=self.button_event)
+        self.button1.grid(row=4, column=0, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+    def button_event(self):
+        print("button pressed")
+        self.plant = (self.plant+1)%len(self.db)
+        self.button1 = customtkinter.CTkButton(master=self, text=self.db[self.plant][0],image=self.db[self.plant][4], command=self.button_event)
+        self.button1.grid(row=4, column=0, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+        self.idealH = self.db[self.plant][1]
+        self.idealT = self.db[self.plant][2]
+        self.idealL = self.db[self.plant][3]
         
+        self.label7 = customtkinter.CTkLabel(master=self, text="Required Humidity : " + str(round(self.idealH,2)))
+        self.label7.grid(row=4, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+        self.label8 = customtkinter.CTkLabel(master=self, text="Required Temperature : " + str(round(self.idealT,2)))
+        self.label8.grid(row=5, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+        self.label9 = customtkinter.CTkLabel(master=self, text="Required Light : " + str(round(self.idealL,2)))
+        self.label9.grid(row=6, column=1, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+
+
+
+
     def slider_humid(self, value):
         self.humid = value
         self.Chumid = self.idealH - self.humid
@@ -91,6 +134,7 @@ class App(customtkinter.CTk):
         self.label6.grid(row=3, column=2, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
         self.label3 = customtkinter.CTkLabel(master=self, text="Light : " + str(round(self.light,2)))
         self.label3.grid(row=3, column=0, columnspan=1, padx=20, pady=(20, 0), sticky="nsew")
+    
     
 
 if __name__ == "__main__":
